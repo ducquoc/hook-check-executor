3e5af0d8a15f4217af9cf9e8a7462ab1846ce3f5
ducquoc <ducquoc.vn@gmail.com>
2018-03-30 00:12:12 +0700

    update dq_push_sync_fork_github.sh

--- dq_push_sync_fork_github.sh
+++ dq_push_sync_fork_github.sh
@@ -22,7 +22,7 @@ cd "$BASEDIR"
 GIT_REMOTE_UPSTREAM=$(git remote -v | grep upstream | grep -v push | awk '{print $2}')
 if [ ! -z "$GIT_REMOTE_UPSTREAM" ]; then
   echo -e " \n Will use existing upstream: $GIT_REMOTE_UPSTREAM \n "
-  #git remote remove upstream
+  #git remote rm upstream
   UPSTREAM_URL=$GIT_REMOTE_UPSTREAM
 else 
   echo -e " \n Will add upstream (new ref): $UPSTREAM_URL \n "
@@ -36,8 +36,8 @@ git checkout master && git merge upstream/master
 GIT_STATUS_DIRTY=$(git status --porcelain)
 if [ ! -z "$GIT_STATUS_DIRTY" ]; then
   echo -e " \n What the fork? Italian Malta joke?! \n "
-  #git add . && git commit -a -m "update origin after merge upstream "
-  git add . && git commit -a -m "./dq_push_sync_fork_github.sh git@github.com:ducquoc/hook-check-executor.git"
+  LOCAL_DATE=`date`
+  git add . && git commit -a -m "update origin after merge upstream $LOCAL_DATE "
   git push origin master
 else
   echo -e " \n Nothing new. I want a fork on the table. \n "
