f5c885ad3cad0177f5b52992ae4ade05699ff428
ducquoc <ducquoc.vn@gmail.com>
2018-03-30 01:17:39 +0700

    added script dq_push_multi_origin_github.sh

--- dq_push_multi_origin_github.sh
+++ dq_push_multi_origin_github.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
   echo $0 REMOTE_UPSTREAM_URL [LOCAL_REPO_DIR]
   echo Examples: 
   echo "$0 git@github.com:webflow/leadership.git ../leadership"
-  echo "cd ../leadership && ../hook-check-executor/$SCRIPT_NAME git@github.com:webflow/leadership.git && cd - "
+  echo "cd ../jutil-dq && ../hook-check-executor/$SCRIPT_NAME git@bitbucket.org:ducquoc/jutil-dq.git && cd - "
   exit
 else
   UPSTREAM_URL="$1"
@@ -18,27 +18,29 @@ fi
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 #echo Absolute basedir: $BASEDIR
 
+REMOTE_REF="up_bb"
+
 cd "$BASEDIR"
-GIT_REMOTE_UPSTREAM=$(git remote -v | grep upstream | grep -v push | awk '{print $2}')
+GIT_REMOTE_UPSTREAM=$(git remote -v | grep $REMOTE_REF | grep -v push | awk '{print $2}')
 if [ ! -z "$GIT_REMOTE_UPSTREAM" ]; then
-  echo -e " \n Will use existing upstream: $GIT_REMOTE_UPSTREAM \n "
-  #git remote rm upstream
+  echo -e " \n Will use existing $REMOTE_REF: $GIT_REMOTE_UPSTREAM \n "
+  #git remote rm $REMOTE_REF
   UPSTREAM_URL=$GIT_REMOTE_UPSTREAM
 else 
-  echo -e " \n Will add upstream (new ref): $UPSTREAM_URL \n "
-  git remote add upstream $UPSTREAM_URL
+  echo -e " \n Will add $REMOTE_REF (new ref): $UPSTREAM_URL \n "
+  git remote add $REMOTE_REF $UPSTREAM_URL
 fi
 
-#git pull upstream
-git fetch upstream
-git checkout master && git merge upstream/master
+#git pull $REMOTE_REF
+git fetch $REMOTE_REF
+git checkout master && git merge $REMOTE_REF/master
 
 GIT_STATUS_DIRTY=$(git status --porcelain)
 GIT_STATUS_AHEAD=$(git status | grep "ahead of")
 if [ ! -z "$GIT_STATUS_DIRTY" ] || [ ! -z "$GIT_STATUS_AHEAD" ]; then
   echo -e " \n What the fork? Italian Malta joke?! \n "
   LOCAL_DATE=`date`
-  git add . && git commit -a -m "update origin after merge upstream $LOCAL_DATE "
+  git add . && git commit -a -m "update origin after merge $REMOTE_REF $LOCAL_DATE "
   git push origin master
 else
   echo -e " \n Nothing new. I want a fork on the table. \n "
