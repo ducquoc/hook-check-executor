ac69d49571970cc4144810c2249aa73215b56be2
ducquoc <ducquoc.vn@gmail.com>
2018-03-29 23:53:07 +0700

    ./dq_push_sync_fork_github.sh git@github.com:ducquoc/hook-check-executor.git

--- README.md
+++ README.md
@@ -6,4 +6,39 @@ check/experiment the webhook with BitBucket
 
 2/ hook the commits/push of some other BitBucket repo (such as git@bitbucket.org:ducquoc/jutil-dq.git ), create encrypted logs
 
+3/ hook misc integration/app (Slack, FB, PayPal, ...etc)
+
+. . .
+
+Callback servers might be localhost/Docker/VM, or cloud (Heroku, CloudFoundry, AWS, ...etc).
+
+
+
+
+## TANSTAAFL
+
+Please bear in mind that "There is more than one way to do it" (Perl veterans: TIMTOWTDI "tin tao đi"). Webhook is good, but it's not the only one - there are always some other ways, even the classic old Shell script (Bash, Dash, Csh, Tcsh, Ksh, Zsh, ...) can do. 
+
+See: 
+
+https://ducquoc.wordpress.com/2011/11/19/ssh-without-password/
+```
+https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/
+https://confluence.atlassian.com/bitbucket/set-up-an-ssh-key-728138079.html
+https://docs.gitlab.com/ee/gitlab-basics/create-your-ssh-keys.html
+```
+
+if you want to use the scripts to sync between Git repos: 
+
+    `./dq_push_sync_fork_github.sh`
+ 
+ Example: ducquoc/leadership forked from webflow/leadership 
+
+    `./dq_push_multi_origin_bitbucket.sh`
+
+ Example: `ducquoc/jutil-dq` GitHub sync with `ducquoc/jutil-dq` BitBucket
+
+    `./dq_push_multi_origin_github.sh`
+
+ Example: `ducquoc/jutil-dq` BitBucket sync with `ducquoc/jutil-dq` GitHub
 
---
+++ dq_push_sync_fork_github.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+BASEDIR=`dirname "$0"`
+SCRIPT_NAME=`basename "$0"`
+
+if [ -z "$1" ]; then 
+  echo USAGE: 
+  echo $0 REMOTE_UPSTREAM_URL [LOCAL_REPO_DIR]
+  echo Examples: 
+  echo "$0 git@github.com:webflow/leadership.git ../leadership"
+  echo "cd ../leadership && ../hook-check-executor/$SCRIPT_NAME git@github.com:webflow/leadership.git && cd - "
+  exit
+else
+  UPSTREAM_URL="$1"
+  BASEDIR="$2"
+fi
+
+BASEDIR=`(cd "$BASEDIR"; pwd)`
+#echo Absolute basedir: $BASEDIR
+
+cd "$BASEDIR"
+GIT_REMOTE_UPSTREAM=$(git remote -v | grep upstream | grep -v push | awk '{print $2}')
+if [ ! -z "$GIT_REMOTE_UPSTREAM" ]; then
+  echo -e " \n Will use existing upstream: $GIT_REMOTE_UPSTREAM \n "
+  #git remote remove upstream
+  UPSTREAM_URL=$GIT_REMOTE_UPSTREAM
+else 
+  echo -e " \n Will add upstream (new ref): $UPSTREAM_URL \n "
+  git remote add upstream $UPSTREAM_URL
+fi
+
+#git pull upstream
+git fetch upstream
+git checkout master && git merge upstream/master
+
+GIT_STATUS_DIRTY=$(git status --porcelain)
+if [ ! -z "$GIT_STATUS_DIRTY" ]; then
+  echo -e " \n What the fork? Italian Malta joke?! \n "
+  #git add . && git commit -a -m "update origin after merge upstream "
+  git add . && git commit -a -m "./dq_push_sync_fork_github.sh git@github.com:ducquoc/hook-check-executor.git"
+  git push origin master
+else
+  echo -e " \n Nothing new. I want a fork on the table. \n "
+fi
+
+cd -
+
